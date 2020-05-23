38753c16f9e77fcbe488945f79949de5c3b5eb8b
Dalton <C.Dalton@mitsui.com>
2020-05-23 10:31:48 -0500

    Changing to Reskin WFPL

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.css';
 
 function App() {
-  const [values,setValues] = useState("")
+  //const [values,setValues] = useState("")
   
-  
+  const handleChange = event => {
+    event.preventDefault();
+    console.log(event.target.value);
+  }
   
   
   return (
-<form>
-  <h1>Playground App Form</h1>
-  <label>
-    Value 1:
-    <input type="text" name="name" />
-  </label>
-  <br/>
-  <label>
-    Value 2:
-    <input type="text" name="name" />
-  </label>
-  <br/>
-  <label>
-    Value 3:
-    <input type="text" name="name" />
-  </label>
-  <br/>
-  <br/>
-  <input 
-    type="submit" 
-    value="Submit Values"/>
-    
-</form>
+    <form>
+      <h1>Playground App Form</h1>
+        <label>
+          Value 1:
+          <input type="text" name="name" />
+        </label>
+      <br/>
+      <br/>
+      <input 
+        type="submit" 
+        value="Submit Values"
+        onChange = {handleChange}
+      />
+    </form>
   );
 }
 
