5e9ada71d8a0e430efd6ef8ee72f3945d1f7eeb3
Dalton <C.Dalton@mitsui.com>
2020-05-23 10:05:27 -0500

    Changed UI to match requirements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [values,setValues] = useState("")
+  
+  
+  
+  
   return (
-    <form>
+<form>
+  <h1>Playground App Form</h1>
   <label>
-    Name:
+    Value 1:
     <input type="text" name="name" />
   </label>
-  <input type="submit" value="Submit" />
+  <br/>
+  <label>
+    Value 2:
+    <input type="text" name="name" />
+  </label>
+  <br/>
+  <label>
+    Value 3:
+    <input type="text" name="name" />
+  </label>
+  <br/>
+  <br/>
+  <input 
+    type="submit" 
+    value="Submit Values"/>
+    
 </form>
   );
 }
