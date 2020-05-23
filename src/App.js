c2e614dd50a8f4698ee6a36d933ba72780dd956a
Dalton <C.Dalton@mitsui.com>
2020-05-23 11:21:00 -0500

    ignoring form format

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
-const InputForm = () => {
+const Form = () => {
   const [value, setValue] = React.useState("");
+  //const [value2, setValue2] = React.useState("");
 
   const printValues = event => {
     event.preventDefault();
@@ -14,7 +15,7 @@ const InputForm = () => {
     <form onSubmit={printValues}>
       <h1>Playground App</h1>
       <label>
-        Enter Value:
+        Enter Value 1:
         <input
           value={value}
           onChange={({ target: { value } }) => setValue(value)}
@@ -22,10 +23,8 @@ const InputForm = () => {
       </label>
       <br />
       <button>Submit</button>
-      <br />
-      <p>Value in Box: {value}</p>
     </form>
   );
 };
 
-export default InputForm;
+export default Form;
